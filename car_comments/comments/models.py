65003260648f5afdb8cc6e3293ab3d0ffded173d
Memozir <gizmovap@gmail.com>
2023-03-22 21:22:36 +0300

    added comment models

--- car_comments/comments/models.py
+++ car_comments/comments/models.py
@@ -1,16 +1,35 @@
 from django.db import models
-
+from datetime import datetime
 
 class Country(models.Model):
-    name = models.CharField(verbose_name='Имя', max_length=255, null=True, default='неизвестно')
+    name = models.CharField(verbose_name='Имя', max_length=255, null=False)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Producer(models.Model):
-    name = models.CharField(verbose_name='Имя', max_length=255, default='неизвестно')
-    producer = models.ForeignKey(to=Country, verbose_name='Производитель', on_delete=models.SET_DEFAULT)
+    name = models.CharField(verbose_name='Имя', max_length=255, null=False)
+    country = models.ForeignKey('Country', verbose_name='Производитель', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Car(models.Model):
-    name = models.CharField(verbose_name='Имя', max_length=128)
-    producer = models.ForeignKey(to=Producer, on_delete=models.SET_DEFAULT, default='неизвестно')
-    year_start = models.DateField(verbose_name='Начало производства', null=False)
+    name = models.CharField(verbose_name='Имя', null=False, max_length=128)
+    producer = models.ForeignKey('Producer', on_delete=models.SET_NULL, null=True)
+    year_start = models.PositiveSmallIntegerField(verbose_name='Начало выпуска', null=True, default=datetime.today().year)
+    year_end = models.PositiveSmallIntegerField(verbose_name='Конец выпуска', null=True, default=datetime.today().year)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Comment(models.Model):
+    email = models.EmailField(verbose_name='email', null=False)
+    car = models.ForeignKey('Car', verbose_name='Машина', null=False, on_delete=models.CASCADE)
+    comment_text = models.TextField(verbose_name='Комментарий', max_length=1024, null=False)
+
+    def __str__(self) -> str:
+        return f'{self.email} - {self.car}'
+    
